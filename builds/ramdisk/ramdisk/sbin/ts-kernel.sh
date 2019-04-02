c47ecca71310fd4aaa7458936cf6dcf71e64f13d
ThunderStorms21th <pinakastorm@gmail.com>
2019-04-02 14:21:26 +0200

    added U4CSC4 ramdisk source with initial settings

---
+++ builds/ramdisk/ramdisk/sbin/ts-kernel.sh
@@ -0,0 +1,166 @@
+#!/system/bin/sh
+
+# ThunderStorm bash script for kernel and policy features settings v1.1
+# Thanks to MoRoGoKu and djb77
+
+# Set Variables
+RESETPROP="/sbin/magisk resetprop -v -n"
+
+# Mount
+#mount -o remount,rw -t auto /
+#mount -t rootfs -o remount,rw rootfs
+#mount -o remount,rw -t auto /system
+#mount -o remount,rw /data
+#mount -o remount,rw /cache
+
+# Set KNOX to 0x0 on running /system
+$RESETPROP ro.boot.warranty_bit "0"
+$RESETPROP ro.warranty_bit "0"
+
+# Fix Samsung Related Flags
+$RESETPROP ro.fmp_config "1"
+$RESETPROP ro.boot.fmp_config "1"
+
+# Fix safetynet flags
+$RESETPROP ro.boot.veritymode "enforcing"
+$RESETPROP ro.boot.verifiedbootstate "green"
+$RESETPROP ro.boot.flash.locked "1"
+$RESETPROP ro.boot.ddrinfo "00000001"
+
+# SELinux (0 / 640 = Permissive, 1 / 644 = Enforcing)
+#echo "0" > /sys/fs/selinux/enforce
+#chmod 640 /sys/fs/selinux/enforce
+
+# Google play services wakelock fix
+sleep 1
+su -c "pm enable com.google.android.gms/.update.SystemUpdateActivity"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService$Receiver"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateActivity"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdatePanoActivity"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver"
+
+# init.d
+if [ ! -d /system/etc/init.d ]; then
+	mkdir -p /system/etc/init.d
+fi
+chown -R root.root /system/etc/init.d
+chmod -R 755 /system/etc/init.d
+for FILE in /system/etc/init.d/*; do
+	sh $FILE >/dev/null
+done;
+for FILE2 in /system/etc/init.d/*.sh; do
+	sh $FILE2 >/dev/null
+done;
+
+# Deepsleep fix (@Chainfire)
+#for i in `ls /sys/class/scsi_disk/`; do
+#	cat /sys/class/scsi_disk/$i/write_protect 2>/dev/null | grep 1 >/dev/null
+#	if [ $? -eq 0 ]; then
+#		echo 'temporary none' > /sys/class/scsi_disk/$i/cache_type
+#	fi
+#done;
+
+# Fix personalist.xml
+#if [ ! -f /data/system/users/0/personalist.xml ]; then
+#	touch /data/system/users/0/personalist.xml
+#	chmod 600 /data/system/users/0/personalist.xml
+#	chown system:system /data/system/users/0/personalist.xml
+#fi
+
+# PWMFix (0 = Disabled, 1 = Enabled)
+# echo "0" > /sys/class/lcd/panel/smart_on
+
+# Kernel Panic off
+# echo "0" > /proc/sys/kernel/panic
+
+# Stock CPU Settings
+echo "2288000" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
+echo "416000" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+echo "1586000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo "338000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo "650000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq
+echo "728000" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq
+echo "50000 650000:40000 754000:40000 962000:40000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay
+echo "80000 728000:30000 1040000:40000 1248000:40000" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay
+echo "80 858000:85 1066000:90" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
+echo "75 832000:78 1040000:80 1352000:85" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads
+echo "97" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load
+echo "98" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
+echo "40000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time
+echo "40000" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time
+echo "40000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate
+echo "40000" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate
+echo "20000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_slack
+echo "20000" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_slack
+
+# HMP settings
+echo "760" > /sys/kernel/hmp/up_threshold
+echo "240" > /sys/kernel/hmp/down_threshold
+echo "962000" > /sys/kernel/hmp/down_compensation_high_freq
+echo "858000" > /sys/kernel/hmp/down_compensation_mid_freq
+echo "754000" > /sys/kernel/hmp/down_compensation_low_freq
+
+# Stock GPU Settings
+echo "650" > /sys/devices/14ac0000.mali/max_clock
+echo "112" > /sys/devices/14ac0000.mali/min_clock
+echo "419" /sys/devices/14ac0000.mali/highspeed_clock
+echo "1" /sys/devices/14ac0000.mali/highspeed_delay
+echo "95" /sys/devices/14ac0000.mali/highspeed_load
+
+# I/O sched settings
+echo 'cfq' > /sys/block/sda/queue/scheduler
+echo "640" > /sys/block/sda/queue/read_ahead_kb
+echo 'cfq' > /sys/block/mmcblk0/queue/scheduler
+echo "1024" > /sys/block/mmcblk0/queue/read_ahead_kb
+echo "0" > /sys/block/sda/queue/iostats
+echo "0" > /sys/block/mmcblk0/queue/iostats
+echo "0" > /sys/block/sda/queue/rq_affinity
+echo "0" > /sys/block/mmcblk0/queue/rq_affinity
+echo "368" > /sys/block/sda/queue/nr_requests
+echo "368" > /sys/block/mmcblk0/queue/nr_requests
+
+# LMK
+echo "18920,23552,32256,42472,65536,102400" > /sys/module/lowmemorykiller/parameters/minfree
+
+# SSWAP and Entropy
+echo "50" > /proc/sys/vm/swappiness
+echo "256" > /proc/sys/kernel/random/write_wakeup_threshold
+echo "64" > /proc/sys/kernel/random/read_wakeup_threshold
+echo "500" > /proc/sys/vm/dirty_expire_centisecs
+echo "1000" > /proc/sys/vm/dirty_writeback_centisecs
+
+# CPU BOOST OFF
+# echo "0" > /sys/module/cpu_boost/parameters/input_boost_enabled
+
+# Enable Fingerprint boost
+# echo "0" > /sys/kernel/fp_boost/enabled
+
+# Tweaks: Internet Speed
+echo 'westwood' > /proc/sys/net/ipv4/tcp_congestion_control
+echo "0" > /proc/sys/net/ipv4/tcp_timestamps
+echo "1" > /proc/sys/net/ipv4/tcp_tw_reuse
+echo "1" > /proc/sys/net/ipv4/tcp_sack
+echo "1" > /proc/sys/net/ipv4/tcp_tw_recycle
+echo "1" > /proc/sys/net/ipv4/tcp_window_scaling
+echo "5" > /proc/sys/net/ipv4/tcp_keepalive_probes
+echo "30" > /proc/sys/net/ipv4/tcp_keepalive_intvl
+echo "30" > /proc/sys/net/ipv4/tcp_fin_timeout
+echo "404480" > /proc/sys/net/core/wmem_max
+echo "404480" > /proc/sys/net/core/rmem_max
+echo "256960" > /proc/sys/net/core/rmem_default
+echo "256960" > /proc/sys/net/core/wmem_default
+echo "4096,16384,404480" > /proc/sys/net/ipv4/tcp_wmem
+echo "4096,87380,404480" > /proc/sys/net/ipv4/tcp_rmem
+
+# Unmount
+#mount -o remount,ro -t auto /
+#mount -t rootfs -o remount,ro rootfs
+#mount -o remount,ro -t auto /system
+#mount -o remount,rw /data
+#mount -o remount,rw /cache
+
