0ebee84bf3567c57a23b918793d342fcdac14319
ThunderStorms21th <pinakastorm@gmail.com>
2020-01-10 12:00:36 +0100

    drivers/base : core.c - fix compile error
    fix commit error
    cpufreq: Avoid cpufreq_suspend() deadlock on system shutdown

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -1951,9 +1951,6 @@ void device_shutdown(void)
 {
 	struct device *dev, *parent;
 
-	wait_for_device_probe();
-	device_block_probing();
-
 	cpufreq_suspend();
 
 	spin_lock(&devices_kset->list_lock);
