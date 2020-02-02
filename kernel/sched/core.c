313e4a7d1ba3580abb6ca97b044e6e5c25c5c31f
ThunderStorms21th <pinakastorm@gmail.com>
2020-02-02 18:56:30 +0100

    Compile fix to Revert "sched/topology: Optimize build_group_mask()" commit 209c3de0e3cef32a736e309682ffe2912b070ff7

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6251,6 +6251,7 @@ enum s_alloc {
 static void build_group_mask(struct sched_domain *sd, struct sched_group *sg)
 {
 	const struct cpumask *span = sched_domain_span(sd);
+	const struct cpumask *sg_span = sched_group_cpus(sg);
 	struct sd_data *sdd = sd->private;
 	struct sched_domain *sibling;
 	int i;
